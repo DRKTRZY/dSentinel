b516d9b893c9bd32a0877f143824d21ba5f7348c
DRKTRZY <darkterzy@gmail.com>
2026-06-05 06:58:49 +0000

    Add initial Docker container discovery

--- app/main.py
+++ app/main.py
@@ -4,7 +4,10 @@ client = docker.from_env()
 
 containers = client.containers.list()
 
-print(f"Found {len(containers)} containers")
+print(f"Found {len(containers)} containers\n")
 
 for container in containers:
-    print(container.name)
+    print(f"Name:   {container.name}")
+    print(f"Image:  {container.image.tags}")
+    print(f"Status: {container.status}")
+    print("-" * 40)
